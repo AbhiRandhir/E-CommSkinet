d256398ccfbf63f6185859edd9d7eb527fb5b468
unknown <randhir.abhishek@gmail.com>
2021-04-09 19:09:02 +0530

    End Section 6

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -30,7 +30,8 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products
+        
+             return await _context.Products
                 .Include(p => p.ProductType)
                 .Include(p => p.ProductBrand)
                 .ToListAsync();
